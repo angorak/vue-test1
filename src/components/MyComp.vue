36d8a153343e91c1520d0eec23cf28467d1b6ac6
Tomáš Hošek <angorak@seznam.cz>
2023-02-27 13:12:43 +0100

    init

---
+++ src/components/MyComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-html="tval"></div>
+</template>
+
+<script>
+export default {
+  name: "MyComp",
+  data() {
+    return {
+      tval: 'dalsi text'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
