777ddbf427a763f4eade6025d57a15207e679725
Tomáš Hošek <angorak@seznam.cz>
2023-03-13 13:43:57 +0100

    wip

--- src/components/MyComp.vue
+++ src/components/MyComp.vue
@@ -4,20 +4,34 @@
   <p>
     {{ ok ? 'YES' : 'NO' }}
   </p>
-  <button @click="toggleButton">Toggle</button>
+  <button @click="toggleButton" :style="btnClass">Toggle</button>
+
+  <ul>
+    <li v-for="({message}, index) in someList">{{ index }} - {{ message }}</li>
+  </ul>
+  <div>
+    <input type="range" min="1" max="30" v-model="rangeVal">
+    {{ rangeVal }}
+  </div>
+  <p :class="{'para': ok}">
+    {{ suffixText }}
+  </p>
   <p v-if="ok">
     {{ Math.PI.toPrecision(rangeVal) }}
   </p>
+  <p v-else>Not ok</p>
+  <template v-if="ok">
+    <h2>Blabla</h2>
+  </template>
+  <h2 v-show="ok">Neco</h2>
   <ul>
-    <li v-for="i in [1,2,3]">{{rangeVal}}</li>
+    <li v-for="(value, key, index) in myObject">
+      {{ index }}. {{ key }}: {{ value }}
+    </li>
   </ul>
-  <div>
-    <input type="range"  min="1" max="30" v-model="rangeVal">
-    {{ rangeVal }}
-  </div>
-  <p>
-    {{ suffixText }}
-  </p>
+
+  <span v-for="i in 10">{{ i }}</span>
+
 </template>
 
 <script>
@@ -34,19 +48,35 @@ export default {
       },
       ok: true,
       rangeVal: 1,
+      someList: [{message: 'Foo'}, {message: 'Bar'}],
+      myObject: {
+        title: 'How to do lists in Vue',
+        author: 'Jane Doe',
+        publishedAt: '2016-04-10'
+      }
+
     }
-  },
+  }
+  ,
   methods: {
     toggleButton() {
       this.ok = !this.ok
-   },
+    }
+    ,
     getDate() {
       return new Date()
     }
-  },
+  }
+  ,
   computed: {
     suffixText() {
       return this.inputVal1 + ' some suffix'
+    }
+    ,
+    btnClass() {
+      return {
+        'font-size': 12 + (+this.rangeVal) + 'px'
+      }
     }
   }
 }
@@ -54,5 +84,7 @@ export default {
 </script>
 
 <style scoped>
-
+.para {
+  background-color: aquamarine;
+}
 </style>
